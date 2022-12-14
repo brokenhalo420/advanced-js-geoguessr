2d586ca192af41ec97a05c7e241191e50bda9454
Mario Nikolov <mario.nikolov@kongsbergdigital.com>
2022-12-14 11:57:47 +0200

    adding models

---
+++ models/score.js
@@ -0,0 +1,8 @@
+class Score {
+    constructor(timestamp, points) {
+        this.timestamp = timestamp;
+        this.points = points;
+    }
+}
+
+module.exports({Score});
---
+++ models/user.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(username, email, name, password){
+        this.username = username;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+}
+
+module.exports({User});
