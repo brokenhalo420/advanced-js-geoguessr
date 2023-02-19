52062ebef6fe2f6b940d77d231b82fd1f45044ff
babogdanov <boyan.bogdanov@accedia.com>
2023-02-19 13:17:34 +0200

    add socket.io to server as well as a happy hour with a timeout

--- server/src/utils.js
+++ server/src/utils.js
@@ -3,15 +3,20 @@
   // < 0.2 is ok
   // < 1 is meh
   // > 5 is bad
-const getScoreFromDistance = (distance) => {
-    if(distance < 0.05) {
+
+const getDistance = (p1, p2) => {
+    return Math.sqrt(Math.pow(p1.lat - p2.lat, 2) + Math.pow(p1.lng - p2.lng, 2))
+  }
+const getScoreFromDistance = (distance, scoreModifier) => {
+    let modified = distance / scoreModifier
+    if(modified < 0.05) {
         return "Excellent"
-    } else if (distance < 0.2) {
+    } else if (modified < 0.2) {
         return "Good"
-    } else if (distance < 1) {
+    } else if (modified < 1) {
         return "OK"
     } 
     return "Bad"
 }
 
-module.exports = getScoreFromDistance
+module.exports = { getDistance, getScoreFromDistance }
