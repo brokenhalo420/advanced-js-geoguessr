4d0168df29466da5f418c7b155331de8593c5673
babogdanov <boyan.bogdanov@accedia.com>
2023-02-19 13:17:46 +0200

    add socket.io to client and happy hour handling

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,5 @@ import initializeLogin from "./pages/login"
 
 const root = document.getElementById("root")
 
-const baseUrl = process.env.SERVER_BASE_URL
-let heading = document.createElement("h1")
-fetch(baseUrl)
-  .then((res) => res.json())
-  .then((res) => {
-    heading.textContent = res.message
-    root.appendChild(heading)
-  })
-  initializeLogin(root)
+initializeLogin(root)
 //initialize(root)
